1b15d7647fda2a9d929308fd0e1cbecd87d7b032
Tyler Farhner <tylerfarhner@gmail.com>
2022-05-28 19:57:37 -0700

    Begins CameraWork Page

--- src/pages/CameraWorkPage.js
+++ src/pages/CameraWorkPage.js
@@ -1,3 +1,7 @@
+// ---- ICONS -----
+import CameraWorkIcon from "../components/Icons/CameraWorkIcon"
+import PhotoIcon from "../components/Icons/PhotoIcon"
+
 export default function CameraWorkPage(props) {
     return (
         <div className="CameraWorkPage">
@@ -7,10 +11,28 @@ export default function CameraWorkPage(props) {
                 </div>
                 <div className="Body">
                     <div className="CameraTopDiv">
-                        <p className="CameraTopText">Camera Top Text</p>
+                        <CameraWorkIcon />
+                        <h1 className="CameraTopText">Camera Work</h1>
+                        <p className="CameraTopDescription">High level overview of camera experience and offered services, etc</p>
                     </div>
                     <div className="CameraMainDiv">
-                        <p className="CameraMainText">Camera Main Text</p>
+                        {/* SOFTWARE DEV */}
+                        <div className="Group">
+                        <h1 className="HomeMainText">Software Development</h1>
+                        <div className="GroupInner">
+                        <PhotoIcon />
+                        <p className="GroupDescription">Little blurb here about services pertaining to this category</p>
+                        </div>
+                        </div>
+
+                        {/* CAMERA WORK */}
+                        <div className="Group">
+                        <h1 className="HomeMainText">Camera Work</h1>
+                        <div className="GroupInner">
+                        <p className="GroupDescription">Little blurb here about services pertaining to this category</p>
+                        <PhotoIcon />
+                        </div>
+                        </div>
                     </div>
                 </div>
             </main>
