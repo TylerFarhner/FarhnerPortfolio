a8d19d33b1cbf194f1c5a8454a397e456221f2f8
Tyler Farhner <tylerfarhner@gmail.com>
2022-05-28 20:06:06 -0700

    Begins Coding Page

--- src/pages/CodingPage.js
+++ src/pages/CodingPage.js
@@ -1,16 +1,41 @@
+//  ---- ICONS ----
+import SoftwareDevIcon from '../components/Icons/SoftwareDevIcon'
+import CellPhoneIcon from '../components/Icons/CellPhoneIcon'
+import ComputerIcon from '../components/Icons/ComputerIcon'
+
 export default function CodingPage(props) {
     return (
         <div className="CodingPage">
             <main className="Page">
-                <div className="Header">
-                    <h1>Header</h1>
-                </div>
                 <div className="Body">
                     <div className="CodingTopDiv">
-                        <p className="CodingTopText">Coding Top Text</p>
+                        <SoftwareDevIcon />
+                        <h1 className="CodingTopText">Software Development</h1>
+                        <p>High level overview of tech experience and offered services, etc</p>
                     </div>
                     <div className="CodingMainDiv">
                         <p className="CodingMainText">Coding Main Text</p>
+                        {/* SOFTWARE DEV */}
+                        <div className="Group">
+                        <div className="GroupInner">
+                        <div>
+                        <h2 className="HomeMainText">Mobile Applications</h2>
+                        <CellPhoneIcon />
+                        </div>
+                        <p className="GroupDescription">Description of Mobile based Projects, examples, etc</p>
+                        </div>
+                        </div>
+
+                        {/* CAMERA WORK */}
+                        <div className="Group">
+                        <div className="GroupInner">
+                        <p className="GroupDescription">Description of Web based Projects, examples, etc</p>
+                        <div>
+                        <h2 className="HomeMainText">Websites</h2>
+                        <ComputerIcon />
+                        </div>
+                        </div>
+                        </div>
                     </div>
                 </div>
             </main>
