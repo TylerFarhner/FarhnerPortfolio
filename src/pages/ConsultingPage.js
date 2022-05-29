223341687b7ce41f6d1b9b69ddfb0d30e5c8718d
Tyler Farhner <tylerfarhner@gmail.com>
2022-05-28 20:14:36 -0700

    Begins Consulting Page

--- src/pages/ConsultingPage.js
+++ src/pages/ConsultingPage.js
@@ -1,16 +1,28 @@
+// ---- ICONS ----
+import ConsultingIcon from '../components/Icons/ConsultingIcon'
+import ToolsIcon from '../components/Icons/ToolsIcon'
+
 export default function ConsultingPage(props) {
     return (
         <div className="ConsultingPage">
             <main className="Page">
-                <div className="Header">
-                    <h1>Header</h1>
-                </div>
                 <div className="Body">
                     <div className="ConsultingTopDiv">
-                        <p className="ConsultingTopText">Consulting Top Text</p>
+                        <ConsultingIcon />
+                        <h1 className="ConsultingTopText">Technical Consulting</h1>
+                        <p>Description of consulting solutions for SBO’s looking to incorporate media, marketing, or tech into their business</p>
                     </div>
                     <div className="ConsultingMainDiv">
-                        <p className="ConsultingMainText">Consulting Main Text</p>
+                        {/* SOFTWARE DEV */}
+                        <div className="Group">
+                        <div className="GroupInner">
+                        <div>
+                        <h2 className="HomeMainText">Digital Toolbox</h2>
+                        <ToolsIcon />
+                        </div>
+                        <p className="GroupDescription">BS blurb about right tools, right time, to bring biz into the tech era</p>
+                        </div>
+                        </div>
                     </div>
                 </div>
             </main>
