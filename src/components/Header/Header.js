cb583783a04a78d71a9dca08a0bb22febb7b5547
Tyler Farhner <tylerfarhner@gmail.com>
2022-05-28 21:58:16 -0700

    Fixes Resume Link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,63 @@
-import {  Link } from 'react-router-dom'
-import './Header.css'
+import { Link } from "react-router-dom";
+import "./Header.css";
 
-import InstagramIcon from '../Icons/InstagramIcon'
-import GithubIcon from '../Icons/GithubIcon'
-import ResumeIcon from '../Icons/ResumeIcon'
+import InstagramIcon from "../Icons/InstagramIcon";
+import GithubIcon from "../Icons/GithubIcon";
+import ResumeIcon from "../Icons/ResumeIcon";
 
-export default function Header(){
-    return (
-        <header className="Header">
-            <nav className='HeaderNavBar'>
-                <ul className="NavLinks">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="About">About</Link></li>
-                        <li><Link to="Cameras">Cameras</Link></li>
-                        <li><Link to="Coding">Coding</Link></li>
-                        <li><Link to="Consulting">Consulting</Link></li>
-                </ul>
-            </nav>
-                <div className='ExternalIcons'>
-                    <a className="ExternalIcon" href="https://www.instagram.com/tyfriedrice/" rel="noreferrer" target="_blank"><InstagramIcon /></a>
-                    <a className="ExternalIcon" href="https://github.com/TylerFarhner" rel="noreferrer" target="_blank"><GithubIcon /></a>
-                    <a className="ExternalIcon" href="https://www.linkedin.com/in/tyfarhner/" rel="noreferrer" target="_blank"><ResumeIcon /></a>
-                </div>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header className="Header">
+      <nav className="HeaderNavBar">
+        <ul className="NavLinks">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="About">About</Link>
+          </li>
+          <li>
+            <Link to="Cameras">Cameras</Link>
+          </li>
+          <li>
+            <Link to="Coding">Coding</Link>
+          </li>
+          <li>
+            <Link to="Consulting">Consulting</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="ExternalIcons">
+        {/* INSTAGRAM */}
+        <a
+          className="ExternalIcon"
+          href="https://www.instagram.com/tyfriedrice/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <InstagramIcon />
+        </a>
+
+        {/* GITHUB */}
+        <a
+          className="ExternalIcon"
+          href="https://github.com/TylerFarhner"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <GithubIcon />
+        </a>
+
+        {/* RESUME */}
+        <a
+          className="ExternalIcon"
+          href="https://www.dropbox.com/s/xsvgrztq8hmj72b/FarhnerResume.pdf?dl=0"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <ResumeIcon />
+        </a>
+      </div>
+    </header>
+  );
 }
